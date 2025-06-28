577d2b1a5d4d7728158f0cdc9a9d986fbeb406de
Nednalda <chad.e.landen@gmail.com>
2025-06-28 16:01:40 -0400

    bookbot letter counter

---
+++ main.py
@@ -0,0 +1,35 @@
+
+import sys
+from stats import get_num_words
+from stats import get_book_text
+from stats import character_count
+from stats import sort_on
+from stats import chars_dict_to_sorted_list
+
+def main():
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_path = sys.argv[1]
+
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
+    chars_dict = character_count(text)
+    chars_sorted_list = chars_dict_to_sorted_list(chars_dict)
+    print_report(book_path, num_words, chars_sorted_list)
+
+
+def print_report(book_path, num_words, chars_sorted_list):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    for item in chars_sorted_list:
+        if not item["char"].isalpha():
+            continue
+        print(f"{item['char']}: {item['num']}")
+
+    print("============= END ===============")
+
+main()
